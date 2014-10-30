ccba3af11dcb1ea80a5d7bde5f7932e3ba1adbd8
Jimi Vyas <jimipvyas@gmail.com>
2014-10-30 14:47:47 -0400

    changed :id

--- crm.rb
+++ crm.rb
@@ -20,11 +20,24 @@ end
 get '/contacts/:id' do
 	@contact = $rolodex.find_contact(params[:id].to_i)
 	if @contact
-		erb :show_contact
+		@contact.first_name = params[:first_name]
+		@contact.last_name = params[:last_name]
+		@contact.email = params[:email]
+		@contact.note = params[:note]		
 	else
 		raise Sinatra::NotFound
 	end
 end
+
+get '/contacts/:id/edit' do
+	@contact = $rolodex.find_contact(params[:id].to_i)
+	if @contact
+		erb :edit_contact
+	else
+		raise Sinatra::NotFound
+	end
+end
+
 
 
 get '/contacts' do
